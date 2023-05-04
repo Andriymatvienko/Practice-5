58cc19a829703fdc68584bb0c74e43ebe7079afc
Andriymatvienko <130711488+Andriymatvienko@users.noreply.github.com>
2023-05-05 02:56:48 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Person
+    {
+        protected int birthYear;
+        protected string gender;
+        protected string firstName;
+        protected string lastName;
+
+        public Person(int birthYear, string gender, string firstName, string lastName)
+        {
+            this.birthYear = birthYear;
+            this.gender = gender;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public abstract void DisplayInfo();
+
+        public virtual void CalculateAge()
+        {
+            int age = DateTime.Now.Year - birthYear;
+            string status = age < 18 ? "несовершеннолетний" : "совершеннолетний";
+            Console.WriteLine($"Возраст: {age}, статус: {status}");
+        }
+    }
+
+    class Student : Person
+    {
+        private string specialty;
+        private int course;
+
+        public Student(int birthYear, string gender, string firstName, string lastName, string specialty, int course)
+            : base(birthYear, gender, firstName, lastName)
+        {
+            this.specialty = specialty;
+            this.course = course;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Студент: {firstName} {lastName}");
+            Console.WriteLine($"Специальность: {specialty}, курс: {course}");
+        }
+    }
+
+    class Schoolboy : Person
+    {
+        private int schoolNumber;
+        private int schoolClass;
+
+        public Schoolboy(int birthYear, string gender, string firstName, string lastName, int schoolNumber, int schoolClass)
+            : base(birthYear, gender, firstName, lastName)
+        {
+            this.schoolNumber = schoolNumber;
+            this.schoolClass = schoolClass;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Школьник: {firstName} {lastName}");
+            Console.WriteLine($"Школа №{schoolNumber}, класс {schoolClass}");
+        }
+    }
+
+    class Teacher : Person
+    {
+        private int experience;
+        private string position;
+
+        public Teacher(int birthYear, string gender, string firstName, string lastName, int experience, string position)
+            : base(birthYear, gender, firstName, lastName)
+        {
+            this.experience = experience;
+            this.position = position;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Преподаватель: {firstName} {lastName}");
+            Console.WriteLine($"Стаж работы: {experience} лет, должность: {position}");
+        }
+
+        public override void CalculateAge()
+        {
+            int age = DateTime.Now.Year - birthYear;
+            string status = age >= 60 ? "пенсионер" : "взрослый";
+            Console.WriteLine($"Возраст: {age}, статус: {status}");
+        }
+    }
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person[] people = new Person[3];
+        people[0] = new Student(2000, "мужской", "Иван", "Иванов", "Программирование", 2);
+        people[1] = new Schoolboy(2008, "мужской", "Петр", "Петров", 1, 7);
+        people[2] = new Teacher(1975, "женский", "Анна", "Сидорова", 20, "Доцент");
+
+        foreach (Person person in people)
+        {
+            person.DisplayInfo();
+            person.CalculateAge();
+            Console.WriteLine();
+        }
+    }
+}
